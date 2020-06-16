6bb2ffba4e04458682f485f4f1fe7f9aceed15d8
Tommy <tmth.skp@edu.mercantec.dk>
2020-06-16 12:11:00 +0200

    mouselook

---
+++ MazeShooter1/Assets/CameraFollowSmooth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowSmooth : MonoBehaviour
+{
+
+    public Transform player;
+    private Vector3 velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPosition = player.position;
+        playerPosition.z = transform.position.z;
+
+        var target = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, 0.5f);
+        transform.position = target;
+        
+    }
+}
---
+++ MazeShooter1/Assets/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var input = Input.GetAxis("Horizontal");
+        var input2 = Input.GetAxis("Vertical");   
+        
+        Vector2 position = transform.position;
+        position.x += speed * Time.deltaTime * input;
+        position.y += speed * Time.deltaTime * input2;
+       
+        transform.position = position;
+       
+    }
+}
